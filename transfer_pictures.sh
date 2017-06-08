e3ca68671b422de9d03455172bc0e827d77996e0
tuppsies <TupperwareC@hotmail.com>
2017-06-08 22:11:24 +1000

    Added script to transfer pictures

--- transfer_pictures.sh
+++ transfer_pictures.sh
@@ -4,9 +4,13 @@
 shopt -s nullglob
 
 while true; do
+
+    #rsync -v --remove-source-files -e ssh *.jpg joshua@192.168.43.132:/home/joshua/Desktop/raspberry_pi_pictures/
+
     for f in *.jpg
     do
-        scp $f joshua@192.168.43.132:/home/joshua/Desktop/
+        rsync -v -e ssh $f joshua@192.168.43.132:/home/joshua/Desktop/raspberry_pi_pictures/
         rm $f
     done
+
 done
