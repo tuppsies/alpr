47248dbf2db8a9d9f03bdc5c5e7a8a070e443123
tuppsies <TupperwareC@hotmail.com>
2017-06-18 21:50:21 +1000

    Created file compresses images and saves them externally using ffmpeg

---
+++ compress_pictures.sh
@@ -0,0 +1,16 @@
+# Resources:
+#https://www.digitalocean.com/community/tutorials/how-to-use-sshfs-to-mount-remote-file-systems-over-ssh
+#http://www.raspberrypi-spy.co.uk/2014/05/how-to-mount-a-usb-flash-disk-on-the-raspberry-pi/
+
+#!/bin/bash
+
+while true;
+do
+    sleep 1
+    for f in *.jpg
+    do
+        ffmpeg -loglevel quiet -i $f -r 1 /media/laptop/home/joshua/Desktop/Raspberry_Pi_Images/$f
+        rm $f
+        echo Compressed picture!
+    done
+done
