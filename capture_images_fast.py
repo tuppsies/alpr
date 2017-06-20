b3cd44007a814ec690dcfb3469314046b0097620
tuppsies <TupperwareC@hotmail.com>
2017-06-20 21:46:54 +1000

    Files now save much faster using shutil and added loop for continuous capture

--- capture_images_fast.py
+++ capture_images_fast.py
@@ -2,6 +2,7 @@
 # https://raspberrypi.stackexchange.com/questions/22040/take-images-in-a-short-time-using-the-raspberry-pi-camera-module
 # http://picamera.readthedocs.io/en/release-1.10/api_camera.html
 
+import shutil
 import os
 import io
 import time
@@ -19,38 +20,42 @@ with picamera.PiCamera() as camera:
     camera.framerate = 30
     time.sleep(2)
 
-    print("Beginning capture")
     # Set up 40 in-memory streams
     numPhotos = 10
-    outputs = [io.BytesIO() for i in range(numPhotos)]
 
+    imageCounter = 0 # used to create the image names
+    while(True):
+        start = time.time()
 
-    start = time.time()
+        outputs = [io.BytesIO() for i in range(numPhotos)]
+        # using the video port below brings out worse quality
+        print("Beginning capture")
+        camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
+        finish = time.time()
+        # How fast were we?
+        print('Captured ' + str(numPhotos) + ' images at %.2ffps' % (numPhotos / (finish - start)))
+        print('Time was: ' + str(finish-start))
 
-    # using the video port below brings out worse quality
-    camera.capture_sequence(outputs, 'jpeg', use_video_port=True)
-    finish = time.time()
-    # How fast were we?
-    print('Captured ' + str(numPhotos) + ' images at %.2ffps' % (numPhotos / (finish - start)))
-    print('Time was: ' + str(finish-start))
-
-    # create SSH connection
-    #ssh = paramiko.SSHClient()
-    #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #ssh.connect('192.168.43.132', username='joshua')
+        # create SSH connection
+        #ssh = paramiko.SSHClient()
+        #ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        #ssh.connect('192.168.43.132', username='joshua')
 
 
     
 
-    # then send the data over ssh to main computer
-    # PROCESSING TIME IS HUGE IF IT HAS TO REWRITE THE FILES
-    fileLocation = "/media/laptop/home/joshua/Desktop/Raspberry_Pi_Images/"
-    print("Processing images from buffer")
-    processingStart = time.time()
-    for x in range(0, numPhotos -1):
-        with open(str(x) +".jpg", "wb") as f:
-            print("Completed an image")
-            f.write(outputs[x].getvalue())
+        # then send the data over ssh to main computer
+        # PROCESSING TIME IS HUGE IF IT HAS TO REWRITE THE FILES
+        fileLocation = "/media/laptop/home/joshua/Desktop/Raspberry_Pi_Images/"
+        print("Processing images from buffer")
+        processingStart = time.time()
+        for x in range(0, numPhotos -1):
+            with open(str(imageCounter) +".jpg", "wb") as f:
+                print("made it here")
+                #f.write(outputs[x].getvalue()) # this takes longer than shutil
+                shutil.copyfileobj(outputs[x],f)
+                print("Writing to file " + str(f))
+            imageCounter += 1
 
-    processingEnd = time.time()
-    print("Processing time was " + str(processingEnd - processingStart))
+        processingEnd = time.time()
+        print("Processing time was " + str(processingEnd - processingStart))
