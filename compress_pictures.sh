a2af87bdfb7fd00da8bc376f58ea6f23b6d36bee
tuppsies <TupperwareC@hotmail.com>
2017-06-21 18:14:15 +1000

    Fixed *.jpg bug

--- compress_pictures.sh
+++ compress_pictures.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 # Resources:
 #https://www.digitalocean.com/community/tutorials/how-to-use-sshfs-to-mount-remote-file-systems-over-ssh
 #http://www.raspberrypi-spy.co.uk/2014/05/how-to-mount-a-usb-flash-disk-on-the-raspberry-pi/
 
-#!/bin/bash
 
 # this line stops the program reading it as literally *.jpg
 shopt -s nullglob
@@ -14,6 +14,6 @@ do
     do
         ffmpeg -loglevel quiet -i $f -r 1 /media/laptop/home/joshua/Desktop/Raspberry_Pi_Images/$f
         rm $f
-        echo Compressed picture!
+        echo Compressed and transferred picture!
     done
 done
