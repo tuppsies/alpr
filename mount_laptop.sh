9232e241e6d2cc8fe80d376931dbc9aaafbb2d3b
tuppsies <TupperwareC@hotmail.com>
2017-06-22 17:42:24 +1000

    Added error messaging for arguments

--- mount_laptop.sh
+++ mount_laptop.sh
@@ -1,1 +1,7 @@
+if [ $# -eq 0 ]
+    then
+        echo "ERROR: supply the IP address of the drive to mount"
+        exit
+fi
+
 sudo sshfs -o allow_other,default_permissions joshua@$1:/ /media/laptop
